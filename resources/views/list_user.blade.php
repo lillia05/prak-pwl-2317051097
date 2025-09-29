ebf3e86632f65f9c0d7230f257ca6f515c7b250e
lillia05 <indahlialekok@gmail.com>
2025-09-29 10:29:43 +0700

    benerin databse

---
+++ resources/views/list_user.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>Daftar Pengguna</h1>
+
+    <table>
+        <thead>
+            <tr>
+                <td>ID</td>
+                <td>NAMA</td>
+                <td>NIM</td>
+                <td>KELAS</td>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($users as $user)
+            <tr>
+                <td>{{ $user->id }}</td>
+                <td>{{ $user->nama }}</td>
+                <td>{{ $user->nim }}</td>
+                <td>{{ $user->nama_kelas }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController; #manggil profile controller
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,7 @@ Route::get('/', function () {
 
 Route::get('/profile/{nama}/{npm}/{kelas}', [ProfileController::class, 'profile']);
 Route::get('/profile/{nama}/{npm}/{kelas}/{foto}', [ProfileController::class, 'profile']);
+
+Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
+Route::post('/user', [UserController::class, 'store'])->name('user.store');
